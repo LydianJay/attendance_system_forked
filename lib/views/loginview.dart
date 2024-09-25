8b5c517e904945a7bbb36dddb13fe53e144db6b0
LydianJay <lloydjayedradan@gmail.com>
2024-09-25 17:06:53 +0800

    fixups

--- lib/views/loginview.dart
+++ lib/views/loginview.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_launcher_icons/config/config.dart';
 import 'package:rfid_attendance_system/config/dbconfig.dart';
 import 'package:rfid_attendance_system/controller/auth.dart';
 import 'package:rfid_attendance_system/styles/styles.dart';
